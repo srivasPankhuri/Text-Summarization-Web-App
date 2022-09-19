a9f4550094bcd6de8b77ea22bbe0f921f5e79225
Pankhuri Srivastava <pankh11pantomath@gmail.com>
2022-09-19 10:57:28 +0530

    Changes

--- app.py
+++ app.py
@@ -8,5 +8,13 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')  
 
+@app.route('/summarize',methods=['GET','POST'])
+def summarize():
+    if request.method=='POST':
+        rawtext=request.form['rawtext']
+        summary,original,len_orig,len_summ=summarizer(rawtext)
+
+    return render_template('summary.html',summary=summary,original=original,len_orig=len_orig,len_summ=len_summ)
+
 if __name__=='__main__':
     app.run(debug=True)
